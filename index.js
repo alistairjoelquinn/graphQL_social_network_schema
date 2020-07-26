4b90f820dde235a698eec62953a951b389eb0f52
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-26 12:53:16 +0200

    dummy subscription set up for practising using pubsub

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 import db from './src/db';
 import Query from './src/resolvers/Query';
@@ -6,14 +6,18 @@ import Mutation from './src/resolvers/Mutation';
 import User from './src/resolvers/User';
 import Post from './src/resolvers/Post';
 import Comment from './src/resolvers/Comment';
+import Subscription from './src/resolvers/Subscription';
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
-        Query, Mutation, User, Post, Comment
+        Query, Mutation, User, Post, Comment, Subscription
     },
     context: {
-        db
+        db,
+        pubsub
     }
 });
 
