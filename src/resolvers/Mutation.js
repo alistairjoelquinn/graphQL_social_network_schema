957da5d80bdacd6268df264165dfc76eda0430a7
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-26 21:40:31 +0200

    subscription added or new comments to a post

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -85,7 +85,7 @@ export default {
         }
         return post;
     },
-    createComment(parent, args, { db }, info) {
+    createComment(parent, args, { db, pubsub }, info) {
         const userExists = db.users.some(user => user.id === args.data.author);
         const postExists = db.posts.some(post => post.id === args.data.post);
         if (!postExists || !userExists) {
@@ -96,6 +96,7 @@ export default {
             ...args.data
         }
         db.comments.push(comment);
+        pubsub.publish(`comment_${args.data.post}`, { comment })
         return comment;
     },
     deleteComment(parent, args, { db }, info) {
--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -10,5 +10,14 @@ export default {
 
             return pubsub.asyncIterator('count');
         }
+    },
+    comment: {
+        subscribe(parent, { postId }, { pubsub, db }, info) {
+            const post = db.posts.find(post => post.id === postId);
+            if (!post) {
+                throw new Error('Post not found...');
+            }
+            return pubsub.asyncIterator(`comment_${postId}`);
+        }
     }
 };
