a9d98a0388344e0746dc1146fdc9f4202768fb0b
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-26 22:34:39 +0200

    final pubsub updates for deleting and updating data

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -114,18 +114,27 @@ export default {
             ...args.data
         }
         db.comments.push(comment);
-        pubsub.publish(`comment_${args.data.post}`, { comment })
+        pubsub.publish(`comment_${args.data.post}`, {
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            }
+        });
         return comment;
     },
-    deleteComment(parent, args, { db }, info) {
+    deleteComment(parent, args, { db, pubsub }, info) {
         const commentIndex = db.comments.findIndex(comment => comment.id === args.id);
         if (commentIndex === -1) {
             throw new Error('Comment not found...')
         }
-        const deletedCommets = db.comments.splice(commentIndex, 1);
-        return deletedCommets[0];
+        const [deletedComment] = db.comments.splice(commentIndex, 1);
+        pubsub.publish(`comment_${deletedComment.post}`, {
+            mutation: 'DELETED',
+            data: deletedComment
+        });
+        return deletedComment;
     },
-    updateComment(parent, { id, data }, { db }, info) {
+    updateComment(parent, { id, data }, { db, pubsub }, info) {
         const comment = db.comments.find(comment => comment.id === id);
         if (!comment) {
             throw new Error('Comment not found...');
@@ -133,6 +142,12 @@ export default {
         if (typeof data.text === 'string') {
             comment.text = data.text;
         }
+        pubsub.publish(`comment_${comment.post}`, {
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+        });
         return comment;
     }
 };
