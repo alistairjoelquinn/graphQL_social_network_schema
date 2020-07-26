dd0c628df53062e3f8db4acb8c128c5f0554f3cd
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-26 12:05:02 +0200

    all basic CRUD operations set up for schema

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -105,5 +105,15 @@ export default {
         }
         const deletedCommets = db.comments.splice(commentIndex, 1);
         return deletedCommets[0];
+    },
+    updateComment(parent, { id, data }, { db }, info) {
+        const comment = db.comments.find(comment => comment.id === id);
+        if (!comment) {
+            throw new Error('Comment not found...');
+        }
+        if (typeof data.text === 'string') {
+            comment.text = data.text;
+        }
+        return comment;
     }
 };
